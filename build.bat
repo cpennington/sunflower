9e7c38aed2a8307bbc83f533fde7afb7741432b5
Gabe Mulley <gabe.mulley@gmail.com>
2012-04-14 16:30:19 -0400

    added priority queue to html. require manual build.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\nodejs\node.exe" "C:\coffeescript\bin\coffee" -o build -w game.coffee
+"C:\Program Files (x86)\nodejs\node.exe" "C:\coffeescript\bin\coffee" -o build game.coffee
